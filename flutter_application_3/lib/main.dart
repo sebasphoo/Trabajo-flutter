e04b5338151a415edd73d15199fee4ab10c5f41a
sebastian <sebastiandavidperezhurtado@gmail.com>
2026-07-30 16:09:30 -0500

    trabajo listo

---
+++ flutter_application_3/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "TITULO",
+      home: Scaffold(body: Text("hola")
+      ),
+    );
+  }
+}
